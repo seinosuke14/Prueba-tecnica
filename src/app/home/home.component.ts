2514a8a715e59ddb8a789de37a55f77147263cc1
seinosuke14 <diegobascur9@gmail.com>
2025-04-10 19:20:07 -0400

    cambios visuales

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { SearchersComponent } from '../searchers/searchers.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent,SearchersComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  filtro = '';
+ 
 
+  actualizarFiltro(valor: string) {
+    this.filtro = valor;
+  }
 }
--- src/app/searchers/searchers.component.ts
+++ src/app/searchers/searchers.component.ts
@@ -20,14 +20,15 @@ interface Personaje {
 export class SearchersComponent implements OnChanges {
   @Input() personajeBuscar: string = '';
   personajes: Personaje[] = [];
+  busquedaRealizada: boolean = false;
   private http = inject(HttpClient);
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['personajeBuscar'] && this.personajeBuscar.trim()) {
       const url = `https://rickandmortyapi.com/api/character/?name=${this.personajeBuscar}`;
       this.http.get<{ results: Personaje[] }>(url).subscribe({
-        next: res => this.personajes = res.results,
-        error: () => this.personajes = [],
+        next: res => (this.personajes = res.results, this.busquedaRealizada = true),
+        error: () => (this.personajes = [], this.busquedaRealizada = true),
       });
     }
   }
